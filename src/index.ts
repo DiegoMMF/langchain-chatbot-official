1727c8c07c198db69980b0e5031d830f643dc8f7
DiegoMMF <diegomaldinifreyre@gmail.com>
2024-10-11 17:48:57 -0300

    use fireworks instead of groq (it was down)

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import { ChatGroq } from "@langchain/groq";
+import { ChatFireworks } from "@langchain/community/chat_models/fireworks";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 (async () => {
-  const llm = new ChatGroq({
-    model: "mixtral-8x7b-32768",
+  const llm = new ChatFireworks({
+    model: "accounts/fireworks/models/llama-v3p1-70b-instruct",
     temperature: 0,
   });
 
   const result = await llm.invoke([{ role: "user", content: "Hi im bob" }]);
-
   console.log(result);
+
+  const result2 = await llm.invoke([{ role: "user", content: "Whats my name?" }]);
+  console.log(result2);
 })();
