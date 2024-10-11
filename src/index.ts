95ae43525b514236214d4fea0ded37b74b3badf6
DiegoMMF <diegomaldinifreyre@gmail.com>
2024-10-11 12:50:32 -0300

    first llm.invoke()

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { ChatGroq } from "@langchain/groq";
+import dotenv from "dotenv";
 
-const llm = new ChatGroq({
-  model: "mixtral-8x7b-32768",
-  temperature: 0
-});
+dotenv.config();
+
+(async () => {
+  const llm = new ChatGroq({
+    model: "mixtral-8x7b-32768",
+    temperature: 0,
+  });
+
+  const result = await llm.invoke([{ role: "user", content: "Hi im bob" }]);
+
+  console.log(result);
+})();
